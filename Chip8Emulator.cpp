69ed191ee62676244acc472e5f3f3f11dbd190c6
Dane <marguglio.dane@gmail.com>
2015-08-13 19:49:44 -0400

    Removed redundant increment_pc
    Added it to the decode opcode function again

--- Chip8Emulator.cpp
+++ Chip8Emulator.cpp
@@ -266,10 +266,8 @@ int Chip8Emulator::decodeOpcode()
 	// 1111 0000 0000 0000   0xF000
 	// ABCD 0000 0000 0000   &
 
-	//I dont like this only because jump commands dont increment pointer.
-	//It'd prolly be easier to keep this and just modify those functions though idk.
-    //increment_pc();
 
+    increment_pc();
 
 	//Check first nibble :)
 	switch(opcode & 0xF000)
@@ -307,13 +305,11 @@ int Chip8Emulator::decodeOpcode()
 		//NN opcode & 0x00FF
 		if(registers[opcode & 0x0F00 >>8] == opcode & 0x00FF)
 			increment_pc();
-		increment_pc();
 		break;
 
 	case 0x4000://0x4XNN Skips the next instruction if VX doesn't equal NN.
 		if(registers[opcode & 0x0F00 >>8] != opcode & 0x00FF)
 			increment_pc();
-		increment_pc();
 		break;
 
 	case 0x5000://0x5XY0 Skips the next instruction if VX equals VY.
@@ -321,7 +317,6 @@ int Chip8Emulator::decodeOpcode()
 		//Y opcode & 0x00F0, remove last 4 bits >> 4
 		if (registers[opcode & 0x0F00 >> 8] == registers[opcode & 0x00F0 >> 4])
 			increment_pc();
-		increment_pc();
 		break;
 
 	case 0x6000://0x6XNN Sets VX to NN.
@@ -377,7 +372,6 @@ int Chip8Emulator::decodeOpcode()
 		case 0x0000://0x9XY0 Skips the next instruction if VX doesn't equal VY.
 			if(registers[opcode & 0x0F00 >> 8] != registers[opcode & 0x00F0 >> 4])
 				increment_pc();
-			increment_pc();
 			break;
 		default:
 			opcodeError();
@@ -387,7 +381,6 @@ int Chip8Emulator::decodeOpcode()
 
 	case 0xA000://0xANNN Sets I to the address NNN.
 		index_register = opcode & 0x0FFF;
-		increment_pc();
 		break;
 
 	case 0xB000://0xBNNN Jumps to the address NNN plus V0.
@@ -396,7 +389,6 @@ int Chip8Emulator::decodeOpcode()
 
 	case 0xC000://0xCXNN Sets VX to the result of a bitwise and operation on a random number and NN.
 		registers[opcode & 0x0F00 >> 8] = (opcode & 0x00FF) & (rand()%0xFF);
-		increment_pc();
 		break;
 	
 	//TODO------------------
@@ -410,13 +402,11 @@ int Chip8Emulator::decodeOpcode()
 		case 0x009E://0xEX9E Skips the next instruction if the key stored in VX is pressed.
 			if(registers[opcode & 0x0F00 >> 8] > 0)
 				increment_pc();
-			increment_pc();
 			break;
 
 		case 0x00A1://0xEXA1 Skips the next instruction if the key stored in VX isn't pressed.
 			if(registers[opcode & 0x0F00 >> 8] == 0)
 				increment_pc();
-			increment_pc();
 			break;
 		default:
 			opcodeError();
@@ -429,30 +419,28 @@ int Chip8Emulator::decodeOpcode()
 		{
 		case 0x0007://0xFX07 Sets VX to the value of the delay timer.
 			registers[(opcode & 0x0F00) >> 8] = delay_timer;
-			increment_pc();
 			break;
-
+		/*----------------------------TODO--------------------------------------------------------*/
 		case 0x000A://0xFX0A A key press is awaited, and then stored in VX.
 			break;
 
 		case 0x0015://0xFX15 Sets the delay timer to VX.
 			delay_timer = registers[(opcode & 0x0F00) >> 8];
-			increment_pc();
 			break;
 
 		case 0x0018://0xFX18 Sets the sound timer to VX.
 			sound_timer = registers[(opcode & 0x0F00) >> 8];
-			increment_pc();
 			break;
 
 		case 0x001E://0xFX1E Adds VX to I.
 			//TODO check for overflow..
 			index_register += registers[(opcode & 0x0F00) >> 8];
-			increment_pc();
 			break;
 
+		/*----------------------------TODO--------------------------------------------------------*/
 		case 0x0029://0xFX29 Sets I to the location of the sprite for the character in VX. Characters 0-F (in hexadecimal) are represented by a 4x5 font.
 			break;
+		/*----------------------------TODO--------------------------------------------------------*/
 		case 0x0033://0xFX33 Stores the Binary-coded decimal representation of VX, with the most significant of three digits at the address in I, the middle digit at I plus 1, and the least significant digit at I plus 2. (In other words, take the decimal representation of VX, place the hundreds digit in memory at location in I, the tens digit at location I+1, and the ones digit at location I+2.)
 			break;
 
@@ -461,14 +449,12 @@ int Chip8Emulator::decodeOpcode()
 			{
 				//memory[] = registers[register_counter];
 			}
-			increment_pc();
 			break;
 		case 0x0065://0xFX65 Fills V0 to VX with values from memory starting at address I.
 			for(int register_counter = 0; register_counter < (opcode & 0x0F00 >> 8); register_counter++)
 			{
 			}
 			break;
-			increment_pc();
 		default:
 			opcodeError();
 			break;
