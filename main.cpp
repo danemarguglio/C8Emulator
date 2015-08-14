fe3a91cf3acce7687ec1dacd1549e480103fc103
sideways9 <jawilloughby68@gmail.com>
2015-08-14 17:15:02 -0400

    Small main stuff, playing with other games

--- main.cpp
+++ main.cpp
@@ -5,13 +5,14 @@ int main(int argc, char *argv[])
 {
 	using namespace std;
 	Chip8Emulator chip8emulator;
-	chip8emulator.loadProgram("C8Games\\pong");//hate windows
+	chip8emulator.loadProgram("C8Games\\tank");//hate windows
 	while (1)
 	{
 		chip8emulator.cycleCPU();
 		if (chip8emulator.get_draw()){
 			chip8emulator.debugGraphics();
-			Sleep(10);
+			Sleep(100);
+			system("clear");
 		}
 	}
 	return 0;
